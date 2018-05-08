1626489cd01d12a58dcb380c170e7fc2c10a869e
tkiviharju <tomas.kiviharju@gmail.com>
2018-05-08 13:56:46 +0300

    2.1

---
+++ kurssi/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const luoId = () => Math.floor(Math.random() * 999999)
+
+const Otsikko = (props) =>
+  <div>
+    <h1>{props.nimi}</h1>
+  </div>
+
+const Osa = (props) =>
+  <p>
+    {props.osa} {props.tehtavia}
+  </p>
+
+const Sisalto = ({osat}) => {
+  const rivit = osat.map(osa => <Osa key={luoId()} osa={osa.nimi} tehtavia={osa.tehtavia} />)
+  return (
+    <div>
+      {rivit}
+    </div>
+  )
+}
+
+
+const Kurssi = ({kurssi}) =>
+  <div>
+    <Otsikko nimi={kurssi.nimi}/>
+    <Sisalto osat={kurssi.osat}/>
+  </div>
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Kurssi kurssi={kurssi}/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
