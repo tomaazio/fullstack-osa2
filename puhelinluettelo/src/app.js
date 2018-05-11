6ebf4561600ee7f6ad9b5776d2ae20c60d6a61f7
tkiviharju <tomas.kiviharju@gmail.com>
2018-05-12 01:09:04 +0300

    2.11

--- puhelinluettelo/src/app.js
+++ puhelinluettelo/src/app.js
@@ -2,23 +2,29 @@ import React from 'react';
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import Form from './components/Form'
+import axios from 'axios'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber: '',
       newSearch: ''
 
     }
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        this.setState({
+          persons: res.data
+        })
+      })
   }
 
   addPerson = (event) => {
